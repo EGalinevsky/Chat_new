e3a485df5e61d5cdc9dd06dae0c5aa626d0e3be1
Eugene Galinevsky <65033002+EGalinevsky@users.noreply.github.com>
2023-01-28 17:34:39 +0300

    correct commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and sa1ve to reload.
+          Edit <code>src/App1.js</code> and sa1ve to reload.
         </p>
         <a
           className="App-link"
